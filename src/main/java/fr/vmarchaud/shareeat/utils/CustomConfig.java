58b5f915e0eec5336ae65b32509c7683dd24e768
vmarchaud - ThisIsMac <thisismac47@gmail.com>
2016-05-02 20:59:44 +0200

    just add some doc to the config

--- src/main/java/fr/vmarchaud/shareeat/utils/CustomConfig.java
+++ src/main/java/fr/vmarchaud/shareeat/utils/CustomConfig.java
@@ -3,21 +3,19 @@ package fr.vmarchaud.shareeat.utils;
 import fr.vmarchaud.shareeat.enums.EnumEnv;
 
 public class CustomConfig {
-	// PROD
-
-	/*
-	public EnumEnv	ENV 		=	EnumEnv.PROD;
-	public String 	BASE_URL	=	"http://164.132.103.92:8080";
-	public String	HOSTNAME	=	"jdbc:mysql://localhost:3306/" + ENV.toString().toLowerCase();
-	public String	USERNAME	=	"prod_user";
-	public String	PASSWORD	=	"";
-	// DEV */
+	
+	// Actually it only use to choose the db (dev or prod)
 	public static EnumEnv	ENV 		=	EnumEnv.DEV;
-	//public static String 	BASE_URL	=	"http://localhost:4242";
-	public static String 	BASE_URL	=	"http://164.132.103.92:8080";
-	public static String	HOSTNAME	=	"jdbc:mysql://shareeat.vmarchaud.fr:3306/" + ENV.toString().toLowerCase();
-	public static String	USERNAME	=	"dev_user";
-	public static String	PASSWORD	=	"Nc6tqcEy2ZcYBrQP"; // "HMPTEWYSJG";
 	
+	// The url that the http server will match for any request.
+	public static String 	BASE_URL	=	"http://localhost:4242";
 	
+	// The DSN string with driver type / db url / db name
+	public static String	HOSTNAME	=	"jdbc:mysql://localhost:3306/" + ENV.toString().toLowerCase();
+	
+	// Username used to login into the database
+	public static String	USERNAME	=	"user";
+	
+	// Password used to login into the database
+	public static String	PASSWORD	=	"pwd";
 }
